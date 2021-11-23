8d5786a92546f5ecddfee96300d0e1502b8850f5
Nishil Patel <nishilpatel@cloud.upwork.com>
2021-11-23 16:32:30 +0530

    Added One Method

---
+++ src/main/java/Browser.java
@@ -0,0 +1,19 @@
+package main.java;
+
+
+import org.openqa.selenium.WebDriver;
+
+public class Browser {
+    WebDriver driver;
+
+    Browser(WebDriver dri) {
+        this.driver = dri;
+    }
+
+
+    public void openURL(String url) {
+        driver.get(url);
+    }
+
+}
+
