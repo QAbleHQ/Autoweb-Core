898f98c0c1802cb966c5fc0e88d8fc30eb056720
Nishil Patel <nishilpatel@cloud.upwork.com>
2021-11-30 16:49:51 +0530

    Added More wait methods

--- src/main/java/io/unity/autoweb/Wait.java
+++ src/main/java/io/unity/autoweb/Wait.java
@@ -6,6 +6,8 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Wait {
 
@@ -41,19 +43,20 @@ public class Wait {
 
     public void wait_until_all_element_is_disappear(String locator_value) {
         new WebDriverWait(driver, Duration.ofSeconds(60))
-                .until(ExpectedConditions.invisibilityOfAllElements(((element.find(locator_value)))));
+                .until(ExpectedConditions.invisibilityOfAllElements((element.find(locator_value))));
     }
 
-    public void wait_until_alert_is_present(String locator_value) {
+    public void wait_until_alert_is_present() {
         new WebDriverWait(driver, Duration.ofSeconds(60))
                 .until(ExpectedConditions.alertIsPresent());
     }
 
     public void wait_for_second(int second) {
         try {
-            Thread.sleep(second * 1000);
+            Thread.sleep((long)second * 1000);
         } catch (InterruptedException e) {
             e.printStackTrace();
+
         }
     }
 }
